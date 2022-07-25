347b9ba9c8c0964e1858b1782a3f15564f45a296
abouman76 <abouman76@gmail.com>
2022-07-25 15:37:29 +0200

    did 3 exercises

--- cypress/e2e/spec03.cy.js
+++ cypress/e2e/spec03.cy.js
@@ -3,6 +3,17 @@
 it('shows the fruit returned from the test', () => {
   // stub the network call the application makes
   // to the server using "GET /fruit"
+    const fruit = 'Kiwi'
+    cy.intercept('GET', '/fruit', { fruit }).as('selectedFruit');
+    cy.visit('/')
+
+    cy.wait('@selectedFruit');
+
+    // cy.get('#fruit')
+    //     .should('contain', fruit);
+
+    cy.contains('#fruit', fruit);
+
   // return "Kiwi" json object
   // tip: use https://on.cypress.io/intercept
   //
