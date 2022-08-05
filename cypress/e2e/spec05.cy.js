613eb490f66a64fce3b156d251eda325afe6e030
abouman76 <abouman76@gmail.com>
2022-08-05 14:33:34 +0200

    spec 5 done

--- cypress/e2e/spec05.cy.js
+++ cypress/e2e/spec05.cy.js
@@ -3,11 +3,22 @@
 it('shows a different fruit after reloading the page', () => {
   // visit the site using https://on.cypress.io/visit
   // grab the fruit name from the page
+    cy.visit('/');
+
+
   // (make sure it is not "loading...")
-  //
-  // reload the page using https://on.cypress.io/reload
-  // grab the fruit name from the page again
-  // confirm the fruit name is different
-  //
+    cy.get('#fruit')
+        .should('not.include.text', 'loading...')
+        .invoke('text')
+        .then(cy.log)
+        .then((fruit) => {
+        // reload the page using https://on.cypress.io/reload
+            cy.reload()
+        // grab the fruit name from the page again
+        // confirm the fruit name is different
+                cy.get('#fruit')
+                .should('not.contain.text', 'loading...')
+                .and('not.have.text', fruit)
+        })
   // tip: use nested https://on.cypress.io/then callbacks
 })
